63a65b6cee8e5aea3512a6df9ab2418f71205b7e
sandboxSamuel <134462089+sandboxSamuel@users.noreply.github.com>
2024-10-10 10:51:45 -0400

    Add project files.

---
+++ gP-calculateGPA/Program.cs
@@ -0,0 +1,29 @@
+﻿// I have changed the info from the original lesson to "own it" a little more
+
+string studentName = "Daufenmire McTaggert";
+string course1Name = "Herbalism 101";
+string course2Name = "Survival 101";
+string course3Name = "Monstors of the Wild 101";
+string course4Name = "Nature Science I";
+string course5Name = "The Psychology of Battle 101";
+
+int course1Credit = 3;
+int course2Credit = 3;
+int course3Credit = 4;
+int course4Credit = 4;
+int course5Credit = 3;
+
+int gradeA = 4;
+int gradeB = 3;
+
+int course1Grade = gradeA;
+int course2Grade = gradeB;
+int course3Grade = gradeB;
+int course4Grade = gradeB;
+int course5Grade = gradeA;
+
+Console.WriteLine($"{course1Name} {course1Grade}");
+Console.WriteLine($"{course2Name} {course2Grade}");
+Console.WriteLine($"{course3Name} {course3Grade}");
+Console.WriteLine($"{course4Name} {course4Grade}");
+Console.WriteLine($"{course5Name} {course5Grade}");
