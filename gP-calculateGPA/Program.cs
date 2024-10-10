22c45cb49e98d0b32bd11e52e4e6514ce72d39e9
sandboxSamuel <134462089+sandboxSamuel@users.noreply.github.com>
2024-10-10 13:25:31 -0400

    added calculatino for total credit hours and total grade points

--- gP-calculateGPA/Program.cs
+++ gP-calculateGPA/Program.cs
@@ -1,29 +1,55 @@
 ﻿// I have changed the info from the original lesson to "own it" a little more
 
-string studentName = "Daufenmire McTaggert";
-string course1Name = "Herbalism 101";
-string course2Name = "Survival 101";
-string course3Name = "Monstors of the Wild 101";
-string course4Name = "Nature Science I";
-string course5Name = "The Psychology of Battle 101";
+using System;
 
-int course1Credit = 3;
-int course2Credit = 3;
-int course3Credit = 4;
-int course4Credit = 4;
-int course5Credit = 3;
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        string studentName = "Daufenmire McTaggert";
+        string course1Name = "Herbalism 101";
+        string course2Name = "Survival 101";
+        string course3Name = "Monstors of the Wild 101";
+        string course4Name = "Nature Science I";
+        string course5Name = "The Psychology of Battle 101";
 
-int gradeA = 4;
-int gradeB = 3;
+        int course1Credit = 3;
+        int course2Credit = 3;
+        int course3Credit = 4;
+        int course4Credit = 4;
+        int course5Credit = 3;
 
-int course1Grade = gradeA;
-int course2Grade = gradeB;
-int course3Grade = gradeB;
-int course4Grade = gradeB;
-int course5Grade = gradeA;
+        int gradeA = 4;
+        int gradeB = 3;
 
-Console.WriteLine($"{course1Name} {course1Grade} {course1Credit}");
-Console.WriteLine($"{course2Name} {course2Grade} {course2Credit}");
-Console.WriteLine($"{course3Name} {course3Grade} {course3Credit}");
-Console.WriteLine($"{course4Name} {course4Grade} {course4Credit}");
-Console.WriteLine($"{course5Name} {course5Grade} {course5Credit}");
+        int course1Grade = gradeA;
+        int course2Grade = gradeB;
+        int course3Grade = gradeB;
+        int course4Grade = gradeB;
+        int course5Grade = gradeA;
+
+        int totalCreditHours = 0;
+        totalCreditHours += course1Credit;
+        totalCreditHours += course2Credit;
+        totalCreditHours += course3Credit;
+        totalCreditHours += course4Credit;
+        totalCreditHours += course5Credit;
+
+        // I could have written the above as totalCreditHours = course1Credit + course2Credit + and so on which to me makes more sense but the guide recommends the above
+
+        int totalGradePoints = 0;
+        totalGradePoints += course1Credit * course1Grade;
+        totalGradePoints += course2Credit * course2Grade;
+        totalGradePoints += course3Credit * course3Grade;
+        totalGradePoints += course4Credit * course4Grade;
+        totalGradePoints += course5Credit * course5Grade;
+
+        decimal gradePointAverage = totalGradePoints / totalCreditHours;
+
+        Console.WriteLine($"{course1Name} {course1Grade} {course1Credit}");
+        Console.WriteLine($"{course2Name} {course2Grade} {course2Credit}");
+        Console.WriteLine($"{course3Name} {course3Grade} {course3Credit}");
+        Console.WriteLine($"{course4Name} {course4Grade} {course4Credit}");
+        Console.WriteLine($"{course5Name} {course5Grade} {course5Credit}");
+    }
+}
